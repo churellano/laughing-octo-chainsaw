875098206ff60eef067a58e9ed744130b04890d9
Jeremiah Arellano <jarellano@pnimedia.com>
2021-07-08 09:51:34 -0700

    Code cleanup

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var locationsRouter = require('./routes/locations');
+var locationDetailsRouter = require('./routes/locationDetailsRoutes');
 
 var app = express();
 
@@ -22,6 +23,8 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/locations', locationsRouter);
+app.use('/LocationDetails', locationDetailsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mongoDb/mongoDb.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/gottago';
-
-mongoose.connect(url, { useNewUrlParser: true })
