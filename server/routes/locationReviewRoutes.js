c30f525f484a2efdd369235b4a6fdf1767bb42f9
Jeremiah Arellano <jarellano@pnimedia.com>
2021-07-08 09:54:23 -0700

    Add initial routes

---
+++ server/routes/locationReviewRoutes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// const { findlocationReview, createlocationReview} = require('../queries/locationReviewQueries');
+
+// /* GET LocationReviews listing. */
+// router.get('/:placeId', function(req, res, next) {
+//   const placeId = req.params.placeId;
+//   const locationReview = await findlocationReview(placeId);
+
+//   res.send(locationReview);
+// });
+
+module.exports = router;
