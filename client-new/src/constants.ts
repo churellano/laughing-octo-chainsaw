d76292971175b378c736773e7ae82d881897eb45
Jeremiah Arellano <jarellano@pnimedia.com>
2021-07-12 14:41:32 -0700

    Clean up API URL constants

---
+++ client-new/src/constants.ts
@@ -0,0 +1,5 @@
+export const BASE_API = 'http://localhost:3001/';
+
+export const LOCATION_DETAILS_API = `${BASE_API}LocationDetails/`;
+
+export const LOCATION_REVIEW_API = `${BASE_API}LocationReview/`;
