e5b634e28037678045e235daa4b8b8d470ceca4f
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:44:59 -0700

    Add getNearest method

--- client-new/src/api/LocationDetails.ts
+++ client-new/src/api/LocationDetails.ts
@@ -1,6 +1,7 @@
 import { getData, postData } from '../helpers/http';
 import { ILocationDetails } from '../interfaces/ILocationDetails';
 import { LOCATION_DETAILS_API_URL } from '../constants';
+import { Point } from 'geojson';
 
 const LocationDetailsAPI = {
     /**
@@ -11,6 +12,13 @@ const LocationDetailsAPI = {
     getOne: (placeId: string) => {
         return getData<ILocationDetails>(LOCATION_DETAILS_API_URL + placeId);
     },
+
+    /**
+     * Gets nearest Location Details to a point
+     * @param point
+     * @returns Array<ILocationDetails>
+     */
+    getNearest: (point: Point) => getData<Array<ILocationDetails>>(`${LOCATION_DETAILS_API_URL}getNearestLocationDetails/${point.coordinates[1]},${point.coordinates[0]}`),
 
     /**
      * Creates a new record of a Location
