30630cfecc6cbe2177dbc5f0f07e761c957a19cd
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:46:58 -0700

    Connect login form to backend

--- client-new/src/components/Login/Login.tsx
+++ client-new/src/components/Login/Login.tsx
@@ -1,14 +1,30 @@
-import { Box, Button, Paper, TextField, Typography } from "@material-ui/core";
+ import { Box, Button, Paper, TextField, Typography } from "@material-ui/core";
 import RateReviewIcon from '@material-ui/icons/RateReview';
+import { useEffect } from "react";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login, selectLoggedInUser } from "../../features/user/UserSlice";
 
 interface LoginProps {
     showSignup: () => void
 };
 
 function Login({ showSignup }: LoginProps) {
-    const [emailOrUsername, setEmailOrUsername] = useState('');
+    const dispatch = useDispatch();
+
+    const [identifier, setIdentifier] = useState('');
     const [password, setPassword] = useState('');
+
+    const loggedInUser = useSelector(selectLoggedInUser);
+
+    const handleLogin = () => {
+        dispatch(login({ identifier, password }));
+    }
+
+    if (loggedInUser) {
+        return <Redirect to="/" />
+    }
 
     return (
         <Paper variant='outlined'>
@@ -24,7 +40,7 @@ function Login({ showSignup }: LoginProps) {
                         required
                         label="Email or username"
                         variant="outlined"
-                        onChange={(event) => setEmailOrUsername(event.target.value)}
+                        onChange={e => setIdentifier(e.target.value)}
                     />
                 </Box>
                 <Box m={2}>
@@ -33,14 +49,14 @@ function Login({ showSignup }: LoginProps) {
                         label="Password"
                         type="password"
                         variant="outlined"
-                        onChange={(event) => setPassword(event.target.value)}
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </Box>
             </Box>
 
             <Box p={2} display='flex' justifyContent='space-between'>
                 <Button color='primary' onClick={showSignup}>New user</Button>
-                <Button variant='contained' color='primary'>Login</Button>
+                <Button variant='contained' color='primary' onClick={handleLogin}>Login</Button>
             </Box>
         </Paper>
     )
