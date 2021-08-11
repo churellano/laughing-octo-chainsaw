7e79725d9bec632249af7dca61195b34c9113703
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:56:49 -0700

    Add geojson point property

--- client-new/src/interfaces/ILocationDetails.ts
+++ client-new/src/interfaces/ILocationDetails.ts
@@ -1,10 +1,12 @@
 import { ILocationReview } from "./ILocationReview";
+import { Point } from 'geojson';
 
 export interface ILocationDetails {
-    _id: string;
+    _id: string | null;
     placeId: string;
     name: string;
     address: string;
+    point: Point;
     upvotes: number;
     downvotes: number;
     reviews: Array<ILocationReview>;
