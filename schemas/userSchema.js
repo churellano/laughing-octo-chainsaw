66bd7216914a6f47eb8acd6617df00b33dcbc640
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 16:02:30 -0700

    Add password to User schema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
     username: String,
     email: String,
+    password: String
 });
 
 module.exports = mongoose.model('User', schema, 'User')
