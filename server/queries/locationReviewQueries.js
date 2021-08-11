84db394e19d092a5665c1e54305ada58d7137f7f
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:59:37 -0700

    Add createLocationReview query

--- server/queries/locationReviewQueries.js
+++ server/queries/locationReviewQueries.js
@@ -1,10 +1,12 @@
 // const connectionString =
+const LocationDetails = require('../schemas/locationDetailsSchema');
 const LocationReview = require('../schemas/locationReviewSchema');
 const ObjectId = require('mongoose').Types.ObjectId; 
 
 async function findLocationReviews(locationId) {
+  console.log('locationId', locationId);
   const locationReviews = await LocationReview
-    .find({ locationId: new ObjectId(locationId) })
+    .find({ locationDetails: new ObjectId(locationId) })
     .populate('user')
     .sort({'postedDate': -1})
     .limit(5);
@@ -12,14 +14,45 @@ async function findLocationReviews(locationId) {
   return locationReviews;
 }
 
-async function createLocationReview(locationId, userId, description, recommend) {
-    return new LocationReview({
-      locationId,
-      user: userId,
+async function createLocationReview({ locationDetails, user, description, recommend }) {
+  console.log('createLocationReview', locationDetails, user, description, recommend);
+  let locationDetailsFromDb;
+  let locationReview;
+
+  if (locationDetails._id) {
+    console.log('review existing location')
+    locationDetailsFromDb = LocationDetails.findOne({ _id: new ObjectId(locationDetails._id) });
+    locationReview = await LocationReview.create({
+      locationDetails: new ObjectId(locationDetails._id),
+      user: new ObjectId(user._id),
       description,
       postedDate: new Date(),
       recommend
-    }).save();
+    });
+  } else {
+    console.log('review new location')
+    locationDetailsFromDb = await LocationDetails.create({
+      placeId: locationDetails.placeId,
+      name: locationDetails.name,
+      address: locationDetails.address,
+      lat: locationDetails.lat,
+      lng: locationDetails.lng
+    });
+
+    locationReview = LocationReview.create({
+      locationDetails: new ObjectId(locationDetailsFromDb._id),
+      user: new ObjectId(user._id),
+      description,
+      postedDate: new Date(),
+      recommend
+    });
+  }
+
+  recommend ? locationDetailsFromDb.upvotes++ : locationDetailsFromDb.downvotes++;
+  console.log(locationDetailsFromDb);
+  await locationDetailsFromDb.save();
+
+  return locationReview;
 }
 
 module.exports = {
