367334ee9c4eaf816bfa56ecfe35ca57480e3e73
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:50:33 -0700

    Add greeting for logged in users

--- client-new/src/components/Navbar/Navbar.tsx
+++ client-new/src/components/Navbar/Navbar.tsx
@@ -5,82 +5,47 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { createStyles, fade, makeStyles, Theme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import { selectLoggedInUser } from '../../features/user/UserSlice';
+import { useSelector } from 'react-redux';
+import { Fragment } from 'react';
+import { Button } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
     title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-          display: 'block',
-        },
+      flexGrow: 1,
     },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
+  }),
+);
 
 function Navbar() {
     const classes = useStyles();
 
+    const loggedInUser = useSelector(selectLoggedInUser);
+
+    let endContent;
+    if (loggedInUser) {
+      endContent = <Typography variant="h6" color="inherit">Hello, {loggedInUser.username}</Typography>;
+    } else {
+      endContent = <Button color="inherit" href="/login">Login</Button>;
+    }
+
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu">
+                <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
                     <MenuIcon />
                 </IconButton>
-                <Box display='flex'>
-                    <Typography className={classes.title} variant="h6" color="inherit">GottaGo</Typography>
-                </Box>
-                {/* <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon />
-                    </div>
-                    <InputBase
-                        placeholder="Search…"
-                        classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'search' }}
-                    />
-                </div> */}
+                <Typography className={classes.title} variant="h6" color="inherit">GottaGo</Typography>
+                {endContent}
             </Toolbar>
         </AppBar>
     )
