400377740b783a850b5778a8a11e79077e497535
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-11 15:52:25 -0700

    Add fetchNearestLocationDetails and setSelectedLatLng methods

--- client-new/src/features/locationDetails/LocationDetailsSlice.ts
+++ client-new/src/features/locationDetails/LocationDetailsSlice.ts
@@ -2,19 +2,25 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../../app/store';
 import { ILocationDetails } from '../../interfaces/ILocationDetails';
 import LocationDetailsAPI from '../../api/LocationDetails';
+import { Point } from 'geojson';
+import { LatLng } from 'spherical-geometry-js';
 
 interface LocationDetailsState {
     selectedPlaceId: string | null;
+    selectedLatLng: LatLng | null;
     selectedLocationDetails: ILocationDetails | null;
-    selectedLocationDetailsFromMaps: ILocationDetails | null;
+    selectedLocationDetailsFromMaps: ILocationDetails | null,
+    nearestLocationDetails: Array<ILocationDetails>,
     // status: 'idle' | 'loading' | 'succeeded' | 'failed',
     error: string | null | undefined
 }
 
 const initialState: LocationDetailsState = {
     selectedPlaceId: null,
+    selectedLatLng: null,
     selectedLocationDetails: null,
     selectedLocationDetailsFromMaps: null,
+    nearestLocationDetails: [],
     // status: 'idle',
     error: null
 }
@@ -24,12 +30,21 @@ export const fetchLocationDetails = createAsyncThunk('locationDetails/fetchLocat
     return response;
 });
 
+export const fetchNearestLocationDetails = createAsyncThunk('locationDetails/fetchNearestLocationDetails', async (point: Point) => {
+  const response = await LocationDetailsAPI.getNearest(point);
+  console.log('fetchNearestLocationDetails response', response);
+  return response;
+});
+
 export const locationDetailsSlice = createSlice({
     name: 'locationDetails',
     initialState,
     reducers: {
       setSelectedPlaceId: (state, action: PayloadAction<string>) => {
           state.selectedPlaceId = action.payload;
+      },
+      setSelectedLatLng: (state, action: PayloadAction<LatLng>) => {
+        state.selectedLatLng = action.payload;
       },
       setSelectedLocationDetailsFromMaps: (state, action: PayloadAction<google.maps.places.PlaceResult>) => {
         state.selectedLocationDetailsFromMaps = {
@@ -54,17 +69,28 @@ export const locationDetailsSlice = createSlice({
           .addCase(fetchLocationDetails.rejected, (state, action) => {
             // state.status = 'failed';
             state.error = action.error.message;
+          })
+          .addCase(fetchNearestLocationDetails.fulfilled, (state, action) => {
+            // state.status = 'succeeded';
+            state.nearestLocationDetails = action.payload;
+            state.error = null;
+          })
+          .addCase(fetchNearestLocationDetails.rejected, (state, action) => {
+            // state.status = 'failed';
+            state.error = action.error.message;
           });
       }
 });
 
-export const { setSelectedPlaceId, setSelectedLocationDetailsFromMaps } = locationDetailsSlice.actions;
+export const { setSelectedPlaceId, setSelectedLatLng, setSelectedLocationDetailsFromMaps } = locationDetailsSlice.actions;
 
 export default locationDetailsSlice.reducer;
 
 // Selectors
 export const selectPlaceId = (state: RootState) => state.locationDetails.selectedPlaceId;
+export const selectLatLng = (state: RootState) => state.locationDetails.selectedLatLng;
 export const selectLocationDetails = (state: RootState) => state.locationDetails.selectedLocationDetails;
 export const selectLocationDetailsFromMaps = (state: RootState) => state.locationDetails.selectedLocationDetailsFromMaps;
+export const selectNearestLocationDetails = (state: RootState) => state.locationDetails.nearestLocationDetails;
 // export const selectLocationDetailsStatus = (state: RootState) => state.locationDetails.status;
 export const selectLocationDetailsError =  (state: RootState) => state.locationDetails.error;
