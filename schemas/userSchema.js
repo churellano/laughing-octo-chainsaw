1bf75d518b10fa12125fc0a6cc4bfd7f48ba3993
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2021-08-11 18:41:08 -0700

    Delete schemas directory

--- schemas/userSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
-});
-
-module.exports = mongoose.model('User', schema, 'User')
