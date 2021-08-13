98392036215cc5c8a4e4772e34ea9992b0685454
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-12 17:31:33 -0700

    Add back API keys and backend api url

--- client-new/src/constants.ts
+++ client-new/src/constants.ts
@@ -1,6 +1,6 @@
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || '';
+export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || 'AIzaSyDqMPznHZxoxPx88BZN8AuBEmuOnh4snpE';
 
-export const BASE_API_URL = process.env.BASE_API_URL;
+export const BASE_API_URL = process.env.BASE_API_URL || 'https://gottagoserver.herokuapp.com/';
 
 export const LOCATION_DETAILS_API_URL = `${BASE_API_URL}LocationDetails/`;
 
