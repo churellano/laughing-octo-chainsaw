b65c561233e5757f7c8ef7536023314bc19d3afe
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-17 17:36:42 -0700

    Code cleanup

--- client-new/src/components/NearestList/NearestList.tsx
+++ client-new/src/components/NearestList/NearestList.tsx
@@ -1,13 +1,7 @@
 import { Fragment } from 'react';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import Box from '@material-ui/core/Box';
-import { green } from '@material-ui/core/colors';
-import { red } from '@material-ui/core/colors';
 
 import { computeDistanceBetween } from 'spherical-geometry-js';
 import { Paper } from '@material-ui/core';
@@ -16,14 +10,13 @@ import { useSelector } from 'react-redux';
 import { selectLatLng, selectNearestLocationDetails } from '../../features/locationDetails/LocationDetailsSlice';
 import { ILocationDetails } from '../../interfaces/ILocationDetails';
 import { Point } from 'geojson';
+import NearestListItem from '../NearestListItem';
 
-const decimalToPercent = (decimal: number) => Math.floor(decimal * 100);
 
 function NearestList() {
     const nearestLocationDetails = useSelector(selectNearestLocationDetails);
     const currentLatLng = useSelector(selectLatLng);
 
-    const calculateScore = (locationDetails: ILocationDetails) => locationDetails.upvotes / (locationDetails.upvotes + locationDetails.downvotes);
     const pointToLatLng = (point: Point) => new google.maps.LatLng(point.coordinates[1], point.coordinates[0]);
 
     return (
@@ -36,25 +29,7 @@ function NearestList() {
                     {
                         nearestLocationDetails.map((locationDetails: ILocationDetails, index) => (
                             <Fragment>
-                                <ListItem alignItems="flex-start">
-                                    <ListItemText
-                                        primary={locationDetails.name}
-                                        secondary={
-                                            <Box component='div' display='flex' flexDirection='row' justifyContent='space-between' alignItems='center'>
-                                                <span>{currentLatLng && (computeDistanceBetween(currentLatLng, pointToLatLng(locationDetails.point)) / 1000).toFixed(2)} km</span>
-                                                <div style={{
-                                                    display: 'flex',
-                                                    alignItems: 'center',
-                                                    flexWrap: 'wrap',
-                                                }}>
-                                                    <span>{decimalToPercent(calculateScore(locationDetails))}%</span>
-                                                    {calculateScore(locationDetails) > 0.5 ? <ThumbUpIcon fontSize='small' style={{ color: green[500] }}/> : <ThumbDownIcon fontSize='small' style={{ color: red[500] }}/>}
-                                                </div>
-                                            </Box>
-                                        }
-                                    />
-                                </ListItem>
-                                {/* Only place dividers between list items */}
+                                <NearestListItem locationDetails={locationDetails} distance={currentLatLng && (computeDistanceBetween(currentLatLng, pointToLatLng(locationDetails.point)) / 1000)} />
                                 {(index + 1 === nearestLocationDetails.length) ? null : <Divider />}
                             </Fragment>
                         ))
