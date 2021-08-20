f81cabd55fd8a0a4cd3f24570c7963a660eee251
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-19 20:55:42 -0700

    Add utility class

---
+++ client-new/src/helpers/utility.ts
@@ -0,0 +1,9 @@
+import { Point } from "geojson";
+
+export default class Utility {
+    static calculateScore = (upvotes: number, downvotes: number) => (upvotes) / (upvotes + downvotes);
+
+    static decimalToPercent = (decimal: number) => Math.floor(decimal * 100);
+
+    static pointToLatLng = (point: Point) => new google.maps.LatLng(point.coordinates[1], point.coordinates[0]);
+}
