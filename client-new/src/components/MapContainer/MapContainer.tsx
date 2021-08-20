c0518d662140b559f20bab7a5876548f47e81cf0
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-19 20:53:47 -0700

    Refactor MapContainer into functional component

--- client-new/src/components/MapContainer/MapContainer.tsx
+++ client-new/src/components/MapContainer/MapContainer.tsx
@@ -1,66 +1,77 @@
 import { GOOGLE_API_KEY } from '../../constants';
-import { GoogleMap, LoadScript, } from '@react-google-maps/api';
-import { Component } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { CSSProperties, useEffect, useState } from 'react';
 import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
 import { setSelectedPlaceId, setSelectedLatLng, setSelectedLocationDetailsFromMaps, fetchNearestLocationDetails } from '../../features/locationDetails/LocationDetailsSlice';
 import { clearSelectedLocationReviews } from '../../features/locationReview/LocationReviewSlice';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Box, CircularProgress } from "@material-ui/core";
 
 const containerStyle = {
     width: '100%',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     flexGrow: 1
-};
-  
+} as CSSProperties;
+
 const defaultCenter = {
     lat: 49.246292,
     lng: -123.116226
 };
 
-interface MapState {
-    lat: number;
-    lng: number;
-    map: google.maps.Map | null;
-    placeResult: google.maps.places.PlaceResult | null;
-}
-
 const googleMapsLibrariesToLoad: Libraries = ['geometry', 'places'];
 
-class MapContainer extends Component<any, MapState> {
+function MapContainer() {
+    const dispatch = useDispatch();
+    const [lat, setLat] = useState(defaultCenter.lat);
+    const [lng, setLng] = useState(defaultCenter.lng);
+    const [map, setMap] = useState<google.maps.Map | null>(null);
+    // const [placeResult, setPlaceResult] = useState(null);
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            lat: defaultCenter.lat,
-            lng: defaultCenter.lng,
-            map: null,
-            placeResult: null
-        };
-    }
+    useEffect(() => {
+        // Get location of user in order to center the map
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+                map?.setCenter({ lat, lng });
+                dispatch(fetchNearestLocationDetails({
+                    type: "Point",
+                    coordinates: [position.coords.longitude, position.coords.latitude]
+                }));
+                dispatch(setSelectedLatLng(new google.maps.LatLng(position.coords.latitude, position.coords.longitude)));
+            });
+        }
+    }, []);
 
-    handleClick = (e: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
+    const { isLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: GOOGLE_API_KEY,
+        libraries: googleMapsLibrariesToLoad
+    });
+
+    const handleClick = (e: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
         const lat = e.latLng?.lat();
         const lng = e.latLng?.lng();
         if (e && e.latLng && lat && lng) {
-            this.setState({lat, lng});
-            this.props.fetchNearestLocationDetails({
+            setLat(lat);
+            setLng(lng);
+            dispatch(fetchNearestLocationDetails({
                 type: "Point",
                 coordinates: [lng, lat]
-            });
-            this.props.setSelectedLatLng(new google.maps.LatLng(lat, lng));
+            }));
+            dispatch(setSelectedLatLng(new google.maps.LatLng(lat, lng)));
         }
 
-        if ('placeId' in e) {
-            this.findPlaceById(e.placeId);
+        if ('placeId' in e && e.placeId) {
+            findPlaceById(e.placeId);
         }
     }
 
     // Retrieves information about a named location on Google Maps
-    findPlaceById = (placeId: string | null) => {
+    const findPlaceById = (placeId: string) => {
         console.log('Looking for place with Id: ', placeId);
-        if (google.maps.places && this.state.map) {
-            let service = new google.maps.places.PlacesService(this.state.map);
+        if (google.maps.places && map) {
+            let service = new google.maps.places.PlacesService(map);
             let request = {
                 placeId,
                 fields: ['formatted_address', 'geometry', 'name', 'place_id']
@@ -68,62 +79,32 @@ class MapContainer extends Component<any, MapState> {
 
             service.getDetails(request, (placeResult, status) => {
                 if (status === google.maps.places.PlacesServiceStatus.OK && placeResult) {
-                    this.props.setSelectedPlaceId(placeId);
-                    this.props.setSelectedLocationDetailsFromMaps(placeResult);
-                    this.props.clearSelectedLocationReviews();
-                    this.setState({ placeResult });
+                    dispatch(setSelectedPlaceId(placeId));
+                    dispatch(setSelectedLocationDetailsFromMaps(placeResult));
+                    dispatch(clearSelectedLocationReviews());
+                    // setPlaceResult(placeResult);
                 }
             });
         }
     };
 
-    componentDidMount() {
-        // Get location of user in order to center the map
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                this.setState({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                }, () => {
-                    this.state.map?.setCenter({ lat: this.state.lat, lng: this.state.lng });
-                });
-                this.props.fetchNearestLocationDetails({
-                    type: "Point",
-                    coordinates: [position.coords.longitude, position.coords.latitude]
-                });
-                this.props.setSelectedLatLng(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-            });
-        }
-    }
-    
-    render() {
+    const renderMap = () => {
         return (
-            <LoadScript googleMapsApiKey={GOOGLE_API_KEY} libraries={googleMapsLibrariesToLoad}>
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={{lat: this.state.lat, lng: this.state.lng}}
-                    zoom={15}
-                    onClick={(e: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => this.handleClick(e)}
-                    onLoad={map => this.setState({ map })}
-                >
-                    
-                </GoogleMap>
-            </LoadScript>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={{ lat, lng }}
+                zoom={15}
+                onClick={(e: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => handleClick(e)}
+                onLoad={map => setMap(map)}
+            />
         );
+      }
+
+    if (loadError) {
+        return <div>Map cannot be loaded right now, sorry.</div>
     }
-    
+
+    return isLoaded ? renderMap() : <CircularProgress />
 }
 
-// const mapStateToProps = (state, ownProps) => ({
-//     // ... computed data from state and optionally ownProps
-// })
-  
-const mapDispatchToProps = {
-    setSelectedPlaceId,
-    setSelectedLatLng,
-    setSelectedLocationDetailsFromMaps,
-    clearSelectedLocationReviews,
-    fetchNearestLocationDetails
-}
-
-export default connect(null, mapDispatchToProps)(MapContainer);
+export default MapContainer;
