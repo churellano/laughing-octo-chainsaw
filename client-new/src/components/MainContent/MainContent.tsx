6627e7233000e489cdc3be7ca6bf9b7dbd76245e
Jeremiah Arellano <jarellano@pnimedia.com>
2021-08-19 20:52:05 -0700

    Refactor UI to be responsive for mobile

--- client-new/src/components/MainContent/MainContent.tsx
+++ client-new/src/components/MainContent/MainContent.tsx
@@ -1,19 +1,44 @@
-import { Container } from "@material-ui/core";
+import { Container, createStyles, Grid, makeStyles, Paper, Theme, useTheme } from "@material-ui/core";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import MapContainer from '../MapContainer';
 import NearestList from '../NearestList';
 import Box from '@material-ui/core/Box';
 import LocationDetails from '../LocationDetails';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(2)
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
 function MainContent() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const largeScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
     return (
-        <Container>
-            <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='flex-start' mt={5} style={{ 'height': '85vh'}}>
+        <div className={classes.root}>
+          <Grid container spacing={2} direction={largeScreen ? "row":  "column"}>
+            <Grid item sm={12} md={3} style={{ maxWidth: '100%'}}>
                 <NearestList />
+            </Grid>
+            <Grid item sm={12} md={6}>
                 <MapContainer />
+            </Grid>
+            <Grid item sm={12} md={3}>
                 <LocationDetails />
-            </Box>
-        </Container>
-    );
+            </Grid>
+          </Grid>
+        </div>
+      );
 }
 
 export default MainContent;
